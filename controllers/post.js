61798f835898d22b40548ad0939f89a5a9c4598e
Wu Jane <jane72418@gmail.com>
2024-04-14 19:12:17 +0800

    Add: 處理 /posts 路徑請求

--- controllers/post.js
+++ controllers/post.js
@@ -6,6 +6,10 @@ const posts = {
     const allPosts = await PostModel.find();
     successHandler(res, { data: allPosts });
   },
+  async deleteAllPosts(req, res) {
+    await PostModel.deleteMany({});
+    successHandler(res, { message: "已刪除所有貼文" });
+  },
 };
 
 module.exports = posts;
--- routes/posts.js
+++ routes/posts.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const PostControllers = require("../controllers/post")
 
 router.get("/", PostControllers.getPosts);
+router.delete("/", PostControllers.deleteAllPosts);
 
 module.exports = router;
