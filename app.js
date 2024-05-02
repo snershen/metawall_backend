3c62219db17ab4c22d68108b1b479ffd7c920c0a
Wu Jane <jane72418@gmail.com>
2024-05-02 15:11:31 +0800

    add: 捕捉預期外的錯誤 增加 uncaughtException 捕捉重大 error、unhandledRejection 捕捉 catch

--- app.js
+++ app.js
@@ -1,39 +1,55 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+// const axios = require("axios")
 
-var postsRouter = require('./routes/posts');
-var postRouter = require('./routes/post');
+var postsRouter = require("./routes/posts");
+var postRouter = require("./routes/post");
 
 var app = express();
-require('./connections');
 
-app.use(logger('dev'));
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+require("./connections");
+
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/posts', postsRouter);
-app.use('/post', postRouter);
+app.use("/posts", postsRouter);
+app.use("/post", postRouter);
 // app.use('/users', usersRouter);
 
-app.use(function(req, res){
+app.use(function (req, res) {
   res.status(404).send({
     success: false,
-    message: "路由錯誤"
-  })
-})
+    message: "路由錯誤",
+  });
+});
 
 // 統一管理錯誤
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   err.statusCode = err.statusCode || 500;
   res.status(statusCode).send({
-    "error": err
-  })
-})
+    error: err,
+  });
+});
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log('未捕捉到的 rejection', promise, '原因', err);
+});
+
+// axios.get('https://www.nodfss232')
+//   .then(res=>{
+//     console.log(res)
+//   })
 
 module.exports = app;
--- connections/index.js
+++ connections/index.js
@@ -2,7 +2,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
-const DB = process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD);
+// const DB = process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD);
+const DB = "mongodb://localhost:27017/metawall";
 
 (async () => {
   try {
